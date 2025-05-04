637b3db79162b3735bf4f61359889b9d27f24a66
Madhujeet <madhujeet.tomar@gmail.com>
2025-05-04 12:40:18 +0530

    second commit

--- AIToolApps/app/src/main/java/com/example/aitoolapps/MainActivity.kt
+++ AIToolApps/app/src/main/java/com/example/aitoolapps/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        android.util.Log.d(TAG, "onCreate: Heloo Jagriti")
     }
 }
